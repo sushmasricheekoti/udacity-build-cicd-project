5750aaae200aa28d93460f0c7e35999fd7d2e9e0
root <sushmasricheekoti@gmail.com>
2026-08-29 11:21:39 +0000

    Fix EKS 1.33 and CI/CD AWS region

--- setup/terraform/versions.tf
+++ setup/terraform/versions.tf
@@ -1,13 +1,13 @@
-provider "aws" {
-  region = "us-east-1"
-}
+  provider "aws" {
+    region = "us-east-1"
+  }
 
-terraform {
-  required_version = "1.3.9"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.55.0"
+  terraform {
+    required_version = ">=1.3.9"
+    required_providers {
+      aws = {
+        source  = "hashicorp/aws"
+        version = "6.0.0"
+      }
     }
   }
-}
